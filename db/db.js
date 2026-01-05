a20b4de92dd9dc1fbbf911cdb153fa09771f6a30
noorulhaqueshaikh <noorulhaque560@gmail.com>
2026-01-05 18:37:20 +0530

    changes

--- db/db.js
+++ db/db.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 const connectDB = async() => {
     try {
         
-        await mongoose.connect("mongodb://127.0.0.1:27017/kcf_db")
+        await mongoose.connect(process.env.MONGODB_URL);
         console.log("ConnectedDB☑️")
     } catch (error) {
         console.log(error)
